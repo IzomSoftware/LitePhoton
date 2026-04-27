39344bdbac38bad518632325683c0e3db601d2f3
realstresser <rezaebr3234s4@gmail.com>
2026-04-27 17:21:34 +0300

    Update

--- src/read_util/common.rs
+++ src/read_util/common.rs
@@ -1,6 +1,25 @@
 use crate::input::Input;
 use memmap2::Mmap;
+use thiserror::Error;
 use std::io::{BufWriter, Write};
+use strum_macros::EnumString;
+
+#[derive(Debug, Error)]
+pub enum ReadInputError{
+    #[error("Could not use chunk mode while input is STDIN.")]
+    CannotUseStdinWithChunkMode,
+    #[error("The file is empty.")]
+    EmptyFile,
+}
+
+/// Modes of reading
+/// Uses strum lib to convert Enums into Strings and parse them
+#[derive(Debug, PartialEq, EnumString)]
+#[strum(serialize_all = "lowercase")]
+pub enum Mode {
+    Normal,
+    Chunk,
+}
 
 pub unsafe fn map_file(input: Input) -> std::io::Result<Mmap> {
     unsafe { Mmap::map(&input.open_file()?) }
@@ -17,13 +36,4 @@ where
     W: Sized + Write,
 {
     Ok(writer.flush()?)
-}
-
-pub fn fail<W>(writer: &mut BufWriter<W>, line: &[u8]) -> std::io::Result<()>
-where
-    W: Sized + Write,
-{
-    write_all(writer, line)?;
-    flush(writer)?;
-    Ok(())
 }
