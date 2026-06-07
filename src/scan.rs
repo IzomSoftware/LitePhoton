65857ee9c9cb07f83c9ef4f0a05f15ce83c24514
realstresser <rezaebr3234s4@gmail.com>
2026-06-07 13:00:23 +0000

    Prepare for 3.0.0

--- src/scan.rs
+++ src/scan.rs
@@ -1,8 +1,10 @@
 use std::{
     default,
     io::{Read, Write},
+    sync::{Arc, Mutex},
 };
 
+use rayon::iter::{ParallelBridge, ParallelIterator};
 use regex::bytes::Regex;
 use strum_macros::EnumString;
 
@@ -10,27 +12,29 @@ use crate::{
     input::{Input, InputType},
     matching::{MatchStrategyIterator, Matcher},
     scan,
-    utils::{self, stdout_util::BufWriterImpl},
+    utils::{
+        self,
+        stdout_util::{BufWriterImpl, create_stdout_buf_write},
+    },
 };
 
 /// Concurrency providers
 /// Uses strum lib to convert Enums into Strings and parse them
-#[derive(Debug, Default, PartialEq, EnumString, Clone)]
+#[derive(Debug, PartialEq, EnumString, Clone)]
 #[strum(serialize_all = "lowercase")]
 pub enum ConcurrencyProvider {
-    #[default]
     Rayon,
     StdThread,
 }
-#[derive(Debug, Default, PartialEq, EnumString, Clone)]
+#[derive(Debug, PartialEq, EnumString, Clone)]
+#[strum(serialize_all = "lowercase")]
 pub enum ConcurrencyMethod {
     None,
-    #[default]
     Split,
     Chunk,
 }
 pub struct ScanProperties<'a> {
-    pub input: Box<dyn Input>,
+    pub input: Box<dyn Input + Sync>,
     pub prefix: &'a [u8],
     pub matcher: Matcher,
     pub suffix: &'a [u8],
@@ -143,25 +147,95 @@ impl Scanner for NoneScanner {
     }
 }
 
-pub struct RayonScanner {}
+pub struct RayonScanner {
+    method: ConcurrencyMethod,
+}
 impl Scanner for RayonScanner {
     fn scan(&self, scan_properties: ScanProperties) -> Option<Vec<String>> {
-        None
+        match self.method {
+            ConcurrencyMethod::None => {
+                unimplemented!()
+            }
+            ConcurrencyMethod::Split => {
+                let results: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+                let get = scan_properties.get;
+                let mmap = scan_properties.input.mmap().unwrap();
+                let mmap = &mmap[..];
+
+                mmap.split(|&b| b == b'\n')
+                    .par_bridge()
+                    .filter_map(|line| {
+                        Some(
+                            self.match_line(&scan_properties, line)
+                                .into_iter()
+                                .flatten(),
+                        )
+                    })
+                    .for_each(|iter| {
+                        let mut writer = create_stdout_buf_write();
+                        let match_results = iter;
+                        let mut lock = results.lock().unwrap();
+                        if get {
+                            lock.extend(match_results.map(|b| String::from_utf8_lossy(b).into()));
+                        } else {
+                            for result in match_results {
+                                writer.write_all_with_newline(result).unwrap()
+                            }
+                        }
+                    });
+
+                let lock = results.lock().unwrap();
+                if lock.is_empty() {
+                    None
+                } else {
+                    let vec = lock.to_vec();
+                    Some(vec)
+                }
+            }
+            ConcurrencyMethod::Chunk => {
+                unimplemented!()
+            }
+        }
     }
 }
-pub struct StdThreadScanner {}
+pub struct StdThreadScanner {
+    method: ConcurrencyMethod,
+}
 impl Scanner for StdThreadScanner {
     fn scan(&self, scan_properties: ScanProperties) -> Option<Vec<String>> {
         None
     }
 }
+pub struct ScanMethod {
+    concurrency_method: ConcurrencyMethod,
+    concurrency_provider: ConcurrencyProvider,
+}
+impl ScanMethod {
+    pub fn new(method: ConcurrencyMethod, provider: ConcurrencyProvider) -> Self {
+        ScanMethod {
+            concurrency_method: method,
+            concurrency_provider: provider,
+        }
+    }
+}
 pub struct ScannerBuilder {}
 impl ScannerBuilder {
-    pub fn new(concurrency_method: ConcurrencyMethod) -> Box<dyn Scanner> {
-        match concurrency_method {
+    pub fn new(scan_method: ScanMethod) -> Box<dyn Scanner> {
+        match scan_method.concurrency_method {
             ConcurrencyMethod::None => Box::new(NoneScanner {}),
-            ConcurrencyMethod::Split => unimplemented!(),
-            ConcurrencyMethod::Chunk => unimplemented!(),
+            ConcurrencyMethod::Split => {
+                match scan_method.concurrency_provider {
+                    ConcurrencyProvider::Rayon => {
+                        Box::new(RayonScanner{method: scan_method.concurrency_method})
+                    },
+                    ConcurrencyProvider::StdThread => {
+                        unimplemented!()
+                    },
+                }
+            }
+            ConcurrencyMethod::Chunk => {
+                unimplemented!()
+            }
         }
     }
 }
