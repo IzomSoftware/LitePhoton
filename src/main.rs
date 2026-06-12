3883af2f82c995b1bb1807fca99a4fdc45582a83
BuffOvrFlw <ffrick0001980@proton.me>
2026-06-12 16:14:51 +0000

    Remove numaloc as it backfires

--- src/main.rs
+++ src/main.rs
@@ -19,116 +19,12 @@ use crate::{
 };
 
 mod argument_parser;
-pub mod input;
 mod logger;
+pub mod input;
 pub mod matching;
 pub mod scan;
 pub mod utils;
 
-// /// Entry point
-// #[allow(clippy::collapsible_else_if)]
-// fn main() {
-//     let args = Arguments::setup();
-
-//     let env = Environment::setup(args);
-
-//     logger::setup_logger(env.debug).expect("main.rs:: Cannot setup logger");
-
-//     info!("Starting up LitePhoton with this environment: {:?}", env);
-
-//     if is_tty {
-//         if env.dedup {
-//             let lines = unsafe {
-//                 input::get_input::input(
-//                     method.clone(),
-//                     provider,
-//                     Input::Stdin(()),
-//                     env.stable,
-//                     env.prefix.clone(),
-//                     env.suffix.clone(),
-//                     env.keyword.clone(),
-//                     env.regex.clone(),
-//                 )
-//                 .expect("main.rs: Couldn't read stdin")
-//             };
-
-//             let results = if matches!(method, Method::Simple) {
-//                 input::get_input::simple::dedup_vec(lines)
-//             } else {
-//                 input::get_input::split::rayon::dedup_vec(lines)
-//             };
-
-//             for line in results {
-//                 println!("{line}")
-//             }
-//         } else {
-//             unsafe {
-//                 input::print_input::input(
-//                     method,
-//                     provider,
-//                     Input::Stdin(()),
-//                     env.stable,
-//                     env.prefix.clone(),
-//                     env.suffix.clone(),
-//                     env.keyword.clone(),
-//                     env.regex.clone(),
-//                 )
-//                 .expect("main.rs: Couldn't read stdin")
-//             };
-//         }
-//     } else {
-//         if env.dedup {
-//             let mut lines = vec![];
-
-//             for file in &env.file {
-//                 lines.extend(
-//                     unsafe {
-//                         input::get_input::input(
-//                             method.clone(),
-//                             provider.clone(),
-//                             Input::File(PathBuf::from(file)),
-//                             env.stable,
-//                             env.prefix.clone(),
-//                             env.suffix.clone(),
-//                             env.keyword.clone(),
-//                             env.regex.clone(),
-//                         )
-//                         .expect("main.rs: Couldn't read stdin")
-//                     }
-//                     .iter()
-//                     .cloned(),
-//                 );
-//             }
-
-//             let results = if matches!(method, Method::Simple) {
-//                 input::get_input::simple::dedup_vec(lines)
-//             } else {
-//                 input::get_input::split::rayon::dedup_vec(lines)
-//             };
-//             for line in results {
-//                 println!("{line}")
-//             }
-//         } else {
-//             for file in &env.file {
-//                 unsafe {
-//                     input::print_input::input(
-//                         method.clone(),
-//                         provider.clone(),
-//                         Input::File(PathBuf::from(file)),
-//                         env.stable,
-//                         env.prefix.clone(),
-//                         env.suffix.clone(),
-//                         env.keyword.clone(),
-//                         env.regex.clone(),
-//                     )
-//                     .expect("main.rs: Couldn't read file");
-//                 }
-//             }
-//         }
-//     }
-// }
-// #[global_allocator]
-// static ALLOC: numalloc::NumaAlloc = numalloc::NumaAlloc::new();
 
 fn main() {
     let args = &*ARGUMENTS;
